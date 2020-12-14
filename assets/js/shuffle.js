3166278c045d38cd7ce3bf8056883a40d021119a
Oliver Steele <steele@osteele.com>
2020-12-14 15:37:49 +0800

    Fix shuffling

--- assets/js/shuffle.js
+++ assets/js/shuffle.js
@@ -1,18 +1,23 @@
 function shuffleCards() {
   let parent = document.getElementById("shuffled");
   if (!parent) return;
-  let oldElements = [...parent.children];
-  let newElements = [];
-  while (oldElements.length > 0) {
-    let i = Math.floor(oldElements.length * Math.random());
-    newElements.push(oldElements[i]);
-    oldElements.splice(i, 1);
-  }
-  for (let child of [...parent.children]) {
+  let children = shuffle(parent.children);
+  for (let child of children) {
     parent.removeChild(child);
   }
-  for (let child of newElements) {
+  for (let child of children) {
     parent.appendChild(child);
   }
 }
+
+function shuffle(array) {
+  let source = [...array];
+  let result = [];
+  while (source.length > 0) {
+    let i = Math.floor(source.length * Math.random());
+    result.push(source.splice(i, 1)[0]);
+  }
+  return result;
+}
+
 shuffleCards();
